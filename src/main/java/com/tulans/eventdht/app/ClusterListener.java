4a5ae7152c8f13e26762d3cbfe1064918683d414
Sanket Prakash Tulangekar <Sanket.Prakash@go-mmt.com>
2018-05-10 01:07:57 +0530

    first commit to setup pom.xml + akka setup

---
+++ src/main/java/com/tulans/eventdht/app/ClusterListener.java
@@ -0,0 +1,44 @@
+package com.tulans.eventdht.app;
+
+import akka.actor.AbstractActor;
+import akka.cluster.Cluster;
+import akka.cluster.ClusterEvent;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+
+public class ClusterListener extends AbstractActor{
+
+    LoggingAdapter log = Logging.getLogger(getContext().system(), this);
+    Cluster cluster = Cluster.get(getContext().system());
+
+    //subscribe to cluster changes
+    @Override
+    public void preStart() {
+        cluster.subscribe(self(), ClusterEvent.initialStateAsEvents(),
+                ClusterEvent.MemberEvent.class, ClusterEvent.UnreachableMember.class);
+    }
+
+    //re-subscribe when restart
+    @Override
+    public void postStop() {
+        cluster.unsubscribe(self());
+    }
+
+    @Override
+    public AbstractActor.Receive createReceive() {
+        return receiveBuilder()
+                .match(ClusterEvent.MemberUp.class, mUp -> {
+                    log.info("Member is Up: {}", mUp.member());
+                })
+                .match(ClusterEvent.UnreachableMember.class, mUnreachable -> {
+                    log.info("Member detected as unreachable: {}", mUnreachable.member());
+                })
+                .match(ClusterEvent.MemberRemoved.class, mRemoved -> {
+                    log.info("Member is Removed: {}", mRemoved.member());
+                })
+                .match(ClusterEvent.MemberEvent.class, message -> {
+                    // ignore
+                })
+                .build();
+    }
+}
